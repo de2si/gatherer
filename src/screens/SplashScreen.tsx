a67d6998dfe589a370f190bce741475dfc7e07b5
Deeksha Singh <deeksha.amu@gmail.com>
2024-01-04 11:06:57 +0530

    feat: 🚀 implement splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,39 @@
-import {View, Text} from 'react-native';
+// SplashScreen.tsx
+
 import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+import {ActivityIndicator, Text, useTheme} from 'react-native-paper';
+
+// assets
+import Logo from '@assets/logo.png';
 
 const SplashScreen = () => {
+  const theme = useTheme();
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logoImage} />
+      <Text style={[theme.fonts.headlineLarge]}>Gatherer</Text>
+      <Text style={[theme.fonts.titleLarge, {color: theme.colors.primary}]}>
+        Haritika
+      </Text>
+      <ActivityIndicator size={'large'} style={styles.loadingIndicator} />
     </View>
   );
 };
 
 export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoImage: {
+    width: 80,
+    height: 80,
+  },
+  loadingIndicator: {
+    marginTop: 48,
+  },
+});
