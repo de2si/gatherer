d7c4323a4808387b5bc7f7d4701d5d5b6f53784b
Deeksha Singh <deeksha.amu@gmail.com>
2024-05-08 11:59:46 +0530

    style(screens): 💅 refactor UI and show version
    - 💄 Refactor MoreListScreen to utilize common styles.
    - 💄 Update styles and colors, and rename About item to version picked from package.json
    - 💡 Rename Account item to Profile for clarity.

--- src/screens/More/MoreListScreen.tsx
+++ src/screens/More/MoreListScreen.tsx
@@ -1,8 +1,9 @@
 // MoreScreen.tsx
 
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {View} from 'react-native';
 import {Divider, List, useTheme} from 'react-native-paper';
+import packageJson from '../../../package.json';
 
 //nav
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
@@ -11,68 +12,70 @@ import {MoreStackScreenProps} from '@nav/MoreStack';
 // stores
 import {useAuthStore} from '@hooks/useAuthStore';
 
+// styles
+import {commonStyles, fontStyles, spacingStyles} from '@styles/common';
+
 //types
+import {Style} from 'react-native-paper/lib/typescript/components/List/utils';
 type MoreListScreenProps = NativeStackScreenProps<
   MoreStackScreenProps,
   'MoreList'
 >;
 
-interface RenderIconProps {
-  color?: string;
-  size?: number;
-}
-const renderIcon = (props: RenderIconProps, iconName: string) => (
-  <List.Icon {...props} icon={iconName} />
-);
+const renderIcon = (
+  props: {
+    color: string;
+    style: Style;
+  },
+  iconName: string,
+  color: string,
+) => <List.Icon color={color} icon={iconName} style={props.style} />;
 
 const MoreScreen: React.FC<MoreListScreenProps> = ({navigation}) => {
   const theme = useTheme();
   let logout = useAuthStore(store => store.logout);
 
+  const headerStyles = [{color: theme.colors.primary}, fontStyles.bodyXl];
+  const itemStyles = [{color: theme.colors.tertiary}, fontStyles.bodyXl];
+  const iconColor = theme.colors.tertiary;
+
   return (
-    <View
-      style={[styles.container, {backgroundColor: theme.colors.background}]}>
+    <View style={[commonStyles.flex1, spacingStyles.mh16, spacingStyles.mt16]}>
       <List.Item
-        title="Account"
-        left={props => renderIcon(props, 'account-circle-outline')}
+        title="Profile"
+        left={props => renderIcon(props, 'account-circle-outline', iconColor)}
         onPress={() => navigation.navigate('ProfileDetail', {})}
+        titleStyle={itemStyles}
       />
 
       <Divider />
       <List.Section>
-        <List.Subheader style={{color: theme.colors.primary}}>
-          Personalization
-        </List.Subheader>
+        <List.Subheader style={headerStyles}>Personalization</List.Subheader>
         <List.Item
           title="Theme"
-          left={props => renderIcon(props, 'palette-outline')}
+          left={props => renderIcon(props, 'palette-outline', iconColor)}
           onPress={() => navigation.navigate('ThemeSelect', {})}
+          titleStyle={itemStyles}
         />
       </List.Section>
 
       <Divider />
       <List.Section>
-        <List.Subheader style={{color: theme.colors.primary}}>
-          More
-        </List.Subheader>
+        <List.Subheader style={headerStyles}>More</List.Subheader>
         <List.Item
-          title="About"
-          left={props => renderIcon(props, 'information-outline')}
+          title={`v${packageJson.version}`}
+          left={props => renderIcon(props, 'information-outline', iconColor)}
+          titleStyle={itemStyles}
         />
         <List.Item
           title="Logout"
-          left={props => renderIcon(props, 'logout-variant')}
+          left={props => renderIcon(props, 'logout-variant', iconColor)}
           onPress={logout}
+          titleStyle={itemStyles}
         />
       </List.Section>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default MoreScreen;
